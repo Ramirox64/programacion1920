a1c330b60e25d1a10aca0bcd220415551ec2a943
Ramirox64 <ramirox64@gmail.com>
2019-10-17 20:32:21 -0500

    carga inicial de nuestro proyecto

---
+++ Intro Java/src/intro/java/IntroJava.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intro.java;
+
+/**
+ *
+ * @author Salas
+ */
+public class IntroJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bienvenidos a la programación en Java");   
+        System.out.println("Este código va aser subido en github en el repositor programacion1920");
+        
+    }
+    
+}
+
