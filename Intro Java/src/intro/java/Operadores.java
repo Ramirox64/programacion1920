9db7eb0c9bd39fcaa17317a798fc456b74cf3956
Ramirox64 <ramirox64@gmail.com>
2019-10-18 19:34:12 -0500

    Cambios realizados

---
+++ Intro Java/src/intro/java/Operadores.java
@@ -0,0 +1,30 @@
+/*
+Esta clase va aservir para trabajar con las distintas clases de operadores en Java
+ */
+package intro.java;
+
+/**
+ *
+ * @author Salas
+ */
+public class Operadores {
+      
+    public static void main(String[] args) {
+       //Operadores aritméticos
+       int suma = 14+14;
+       int resta = 20-10;
+       double multiplicacion = 50 * 0.12;
+       int division = 100 / 50;
+       int residuo = 7 % 2;
+       
+        System.out.println("La suma es: " + suma);
+        System.out.println("La resta es: " + resta);
+        System.out.println("La multiplicación es: " + multiplicacion);
+        System.out.println("La división es: " + division);
+        System.out.println("El residuo es: " + residuo);
+        
+        //Creadores de incremento y decremento
+        
+    }
+    
+}
