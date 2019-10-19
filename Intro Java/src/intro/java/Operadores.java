4b42cdffd5629733aa1b339260cae395119f5ad2
Ramirox64 <ramirox64@gmail.com>
2019-10-18 19:55:53 -0500

    Version con operadores de incremento y decremento

--- Intro Java/src/intro/java/Operadores.java
+++ Intro Java/src/intro/java/Operadores.java
@@ -17,14 +17,28 @@ public class Operadores {
        int division = 100 / 50;
        int residuo = 7 % 2;
        
-        System.out.println("La suma es: " + suma);
-        System.out.println("La resta es: " + resta);
-        System.out.println("La multiplicación es: " + multiplicacion);
-        System.out.println("La división es: " + division);
-        System.out.println("El residuo es: " + residuo);
+        System.out.println("   La suma es: " + suma);
+        System.out.println("   La resta es: " + resta);
+        System.out.println("   La multiplicación es: " + multiplicacion);
+        System.out.println("   La división es: " + division);
+        System.out.println("   El residuo es: " + residuo);
         
         //Creadores de incremento y decremento
-        
+       int nota = 15;
+       nota  = 1 + nota;
+        System.out.println("2. OPERADORES DE INCREMENTO Y DECREMENTO");
+        System.out.println("   Incremento como nota como nota = nota +1 es: "+ nota);
+        nota += 1;
+        System.out.println("   Incremento como nota + = 1: "+ nota);
+        nota ++;
+        System.out.println("   Incremento como nota++: "+ nota);
+        nota = nota - 1;
+        System.out.println("Decremento de la variable");
+        System.out.println("Decremento como nota = nota - 1: "+ nota);
+        nota -= 1;
+        System.out.println("Decremento como nota- = 1: "+ nota);
+        nota --;
+        System.out.println("Decremento como nota --: "+ nota);
     }
     
 }
